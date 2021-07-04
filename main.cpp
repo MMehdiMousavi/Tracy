5559891f43dd7daccc373e9f60d441658e2f9bad
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-04 17:20:28 -0400

    first PPM image

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j>=0; --j){
+        std::cerr << "\rScanlines remaining: " << j << ' ' <<std::flush;
         for (int i = 0; i< image_width; ++i){
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,7 +25,9 @@ int main() {
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 
-
         }
     }
+    std::cerr << "\nDone!\n";
+
 }
+
