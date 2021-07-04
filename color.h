e3c63469bb95aacd9ab24770bf044ef233180a33
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-04 17:49:10 -0400

    color class

--- color.h
+++ color.h
@@ -4,11 +4,15 @@
 
 #ifndef RAYTRACER_COLOR_H
 #define RAYTRACER_COLOR_H
+#include "vec3.h"
+#include <iostream>
 
+void write_color(std::ostream &out, color pixel_color) {
+//write RGB value of each color
 
-class color {
-
-};
-
+out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+    << static_cast<int>(255.999 * pixel_color.y()) << ' '
+    << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
 
 #endif //RAYTRACER_COLOR_H
