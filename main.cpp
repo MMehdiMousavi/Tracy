2493b98c8f01ad9d2c36605375643107dba3656e
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-04 17:00:34 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 //
 // Created by Mehdi on 7/4/2021.
 //
+#include <iostream>
+//Creates PPM Image
 
+const int image_width = 256;
+const int image_height = 256;
+
+//Render
+
+std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"
+
+for (int j = image_height)
