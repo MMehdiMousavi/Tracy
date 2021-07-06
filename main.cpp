1a879504e531864b67b0c45ecc1c04044e664964
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-05 20:01:28 -0400

    making Image

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 // Created by Mehdi on 7/4/2021.
 //
 #include <iostream>
+#include "ray.h"
 #include "color.h"
 #include "vec3.h"
 
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t) * color(1.0,1.0,1.0) + t * color(0.5,0.7,1.0);
+}
+
 //Creates PPM Image
 int main() {
+
+
 
     const int image_width = 256;
     const int image_height = 256;
