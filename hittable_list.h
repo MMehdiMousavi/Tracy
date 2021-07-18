2b83e04ee087d36f85996e9184ff7904b6f09399
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-17 21:56:50 -0400

    Create hittable_list.h

---
+++ hittable_list.h
@@ -0,0 +1,14 @@
+//
+// Created by Mehdi on 7/17/2021.
+//
+#pragma once
+#ifndef RAYTRACER_HITTABLE_LIST_H
+#define RAYTRACER_HITTABLE_LIST_H
+
+
+class hittable_list {
+
+};
+
+
+#endif //RAYTRACER_HITTABLE_LIST_H
