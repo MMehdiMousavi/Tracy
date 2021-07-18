15a4e8d7ff81040b0e4ef1e1288acece990ced3c
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-18 09:28:46 -0400

    Update material.h

--- material.h
+++ material.h
@@ -14,5 +14,19 @@ class material {
                 )
     };
 
+    class lambertian : public material {
+    public:
+        lambertian(const color& a) : albedo(a) {}
 
+        virtual bool scatter(
+                const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            auto scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
 
+    public:
+        color albedo;
+
+    };
