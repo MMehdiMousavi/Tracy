7b22ab20afaba7a0ab0e9dd80486fb7d5bd63240
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-18 14:54:02 -0400

    Milestone 1 - Final Render

--- main.cpp
+++ main.cpp
@@ -42,11 +42,11 @@ int main() {
     //World
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.58,0.3,0.0));
-    auto material_center = make_shared<dielectric>(1.3);
+    auto material_ground = make_shared<lambertian>(color(0.235, 0.647, 0.843));
+    auto material_center = make_shared<dielectric>(2.5);
 
     auto material_left = make_shared<metal>(color(0.8,0.8,0.8),0.3);
-    auto material_right = make_shared<metal>(color(0.8,0.6,0.2),1.0);
+    auto material_right = make_shared<metal>(color(0.882, 0.439, 0.117),1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0),100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0,0.0,-1), 0.5, material_center));
@@ -55,7 +55,7 @@ int main() {
 
     // Camera
 
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 50, aspect_ratio);
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 25, aspect_ratio);
 
     // Render
 
