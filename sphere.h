e651063b17aaeb51280c0c77187d3892443dcd5e
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-17 20:06:04 -0400

    Update sphere.h

--- sphere.h
+++ sphere.h
@@ -22,7 +22,29 @@ public:
 
 };
 
+bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
 
+    if (discriminant < 0) return false;
+    auto sqrtd = sqrt(discriminant);
 
+    //find the nearest root that lies in the acceptable range.
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+            root = (-half_b+sqrtd)/a;
+    if (root<t_min || t_max<root)
+            return false;
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    rec.normal = (rec.p - center) / radius;
+
+    return true;
+}
 
 #endif //RAYTRACER_SPHERE_H
