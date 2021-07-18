002123cb6348458cb92511e280169e6d14c11720
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-18 01:01:35 -0400

    gamma correction. bounce limit

--- color.h
+++ color.h
@@ -13,9 +13,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
 
     //Divide the color by the number of samples.
     auto scale = 1.0 / samples_per_pixel;
-    r*=scale;
-    g*=scale;
-    b*=scale;
+    r= sqrt(scale*r);
+    g= sqrt(scale*g);
+    b= sqrt(scale*b);
 
 //write RGB value of each color
 out << static_cast<int>(255.999 * r) << ' '
