2e714a63cc93e714f8b1717f1c6c23807bd37b8d
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-17 22:43:39 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -5,10 +5,26 @@
 #ifndef RAYTRACER_UTILS_H
 #define RAYTRACER_UTILS_H
 
+#include <cmath>
+#include <limits>
+#include <memory>
 
-class utils {
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
 
-};
+//Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
 
+//Utils
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+#include "ray.h"
+#include "vec3.h"
+}
 
 #endif //RAYTRACER_UTILS_H
