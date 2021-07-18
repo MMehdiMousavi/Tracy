765a9844cde370a17a7ab45451a8d40ed492237f
Mehdi <mehdimousavi.redcap@gmail.com>
2021-07-18 12:42:58 -0400

    dielectric materials added

--- main.cpp
+++ main.cpp
@@ -36,21 +36,21 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 640;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 1;
+    const int samples_per_pixel = 50;
     const int bounce_lim = 50;
 
     //World
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.58,0.3,0.0));
-    auto material_center = make_shared<lambertian>(color(0.0,0.3,0.3));
+    auto material_center = make_shared<dielectric>(1.3);
 
     auto material_left = make_shared<metal>(color(0.8,0.8,0.8),0.3);
     auto material_right = make_shared<metal>(color(0.8,0.6,0.2),1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0),100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0,0.0,-1), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,0.0,-1), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,0.0,-1), -0.5, material_left));
     world.add(make_shared<sphere>(point3(1.0,0.0,-1), 0.5, material_right));
 
     // Camera
